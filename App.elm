44a6045089a32452bb564d90db86c244e90657c8
Tomi Harju <thar@fl815.local>
2016-03-31 19:44:32 +0300

    Added view

--- App.elm
+++ App.elm
@@ -1,7 +1,7 @@
-import Html exposing (div, button, text)
+import Html exposing (div, button, text, fromElement)
 import Html.Events exposing (onClick)
 import StartApp.Simple as StartApp
-
+import View exposing (..)
 
 main =
   StartApp.start { model = model, view = view, update = update }
@@ -9,19 +9,6 @@ main =
 
 model = 0
 
+view address model = fromElement View.renderWorld
 
-view address model =
-  div []
-    [ button [ onClick address Decrement ] [ text "-" ]
-    , div [] [ text (toString model) ]
-    , button [ onClick address Increment ] [ text "+" ]
-    ]
-
-
-type Action = Increment | Decrement
-
-
-update action model =
-  case action of
-    Increment -> model + 1
-    Decrement -> model - 1
+update action model = 0
