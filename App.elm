34eebfb9e9daed57be51cddc11dccbbb03213f1e
Touko Vainio-Kaila <touko.vk@gmail.com>
2016-03-31 21:53:30 +0300

    Updated App.elm to use StartApp (instead of StartApp.simple)

--- App.elm
+++ App.elm
@@ -1,12 +1,16 @@
+-- From http://www.elm-tutorial.org/040_effects/startapp_with_effects.html
+module Main (..) where
+
+import Effects exposing (Effects, Never)
 import Html exposing (div, button, text, fromElement)
 import Html.Events exposing (onClick)
-import StartApp.Simple as StartApp
 import SolarSystem as SolarSystem
+import StartApp
+import Task
 import View exposing (..)
 
-main =
-  StartApp.start { model = SolarSystem.initialModel, view = view, update = SolarSystem.update }
 
+view : Signal.Address SolarSystem.Action -> SolarSystem.Model -> Html.Html
 view address model =
   div []
     [
@@ -16,3 +20,32 @@ view address model =
     , button [ onClick address SolarSystem.Tick ] [ text "!" ]
     , fromElement (View.canvas model)
     ]
+
+init : ( SolarSystem.Model, Effects SolarSystem.Action )
+init =
+  (  SolarSystem.initialModel, Effects.none )
+
+
+update : SolarSystem.Action ->  SolarSystem.Model -> ( SolarSystem.Model, Effects.Effects SolarSystem.Action )
+update action model =
+  (SolarSystem.update action model, Effects.none)
+
+
+app : StartApp.App SolarSystem.Model
+app =
+  StartApp.start
+    { init = init
+    , inputs = []
+    , update = update
+    , view = view
+    }
+
+
+main : Signal.Signal Html.Html
+main =
+  app.html
+
+
+port runner : Signal (Task.Task Never ())
+port runner =
+  app.tasks
