a83c61d816bf8ccebd908e98aedd943986f83fd2
Touko Vainio-Kaila <touko.vk@gmail.com>
2016-04-01 17:16:50 +0300

    extended default scale

--- Notes.elm
+++ Notes.elm
@@ -3,7 +3,7 @@ module Notes (..) where
 import Array
 
 defaultScale : Array.Array Float
-defaultScale = Array.fromList [c4, d4, e4, f4, g4, a4, b4, c5, c5, d5, e5, f5, g5, a5, b5, c6]
+defaultScale = Array.fromList [c3, d3, e3, f3, g3, a3, b3, c4, d4, e4, f4, g4, a4, b4, c5, c5, d5, e5, f5, g5, a5, b5, c6]
 
 melody : List Float
 melody =
