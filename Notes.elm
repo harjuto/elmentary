62e4982270b4a1ff6de54face0d8a168722b732d
Touko Vainio-Kaila <touko.vk@gmail.com>
2016-04-01 11:06:06 +0300

    Added notes

---
+++ Notes.elm
@@ -0,0 +1,460 @@
+module Notes (..) where
+
+c0 : Float
+c0 = 16.35
+
+cis0 : Float
+cis0 = 17.32
+
+des0 : Float
+des0 = 17.32
+
+d0 : Float
+d0 = 18.35
+
+dis0 : Float
+dis0 = 19.45
+
+ees0 : Float
+ees0 = 19.45
+
+e0 : Float
+e0 = 20.60
+
+f0 : Float
+f0 = 21.83
+
+fis0 : Float
+fis0 = 23.12
+
+ges0 : Float
+ges0 = 23.12
+
+g0 : Float
+g0 = 24.50
+
+gis0 : Float
+gis0 = 25.96
+
+aes0 : Float
+aes0 = 25.96
+
+a0 : Float
+a0 = 27.50
+
+ais0 : Float
+ais0 = 29.14
+
+bes0 : Float
+bes0 = 29.14
+
+b0 : Float
+b0 = 30.87
+
+c1 : Float
+c1 = 32.70
+
+cis1 : Float
+cis1 = 34.65
+
+des1 : Float
+des1 = 34.65
+
+d1 : Float
+d1 = 36.71
+
+dis1 : Float
+dis1 = 38.89
+
+ees1 : Float
+ees1 = 38.89
+
+e1 : Float
+e1 = 41.20
+
+f1 : Float
+f1 = 43.65
+
+fis1 : Float
+fis1 = 46.25
+
+ges1 : Float
+ges1 = 46.25
+
+g1 : Float
+g1 = 49.00
+
+gis1 : Float
+gis1 = 51.91
+
+aes1 : Float
+aes1 = 51.91
+
+a1 : Float
+a1 = 55.00
+
+ais1 : Float
+ais1 = 58.27
+
+bes1 : Float
+bes1 = 58.27
+
+b1 : Float
+b1 = 61.74
+
+c2 : Float
+c2 = 65.41
+
+cis2 : Float
+cis2 = 69.30
+
+des2 : Float
+des2 = 69.30
+
+d2 : Float
+d2 = 73.42
+
+dis2 : Float
+dis2 = 77.78
+
+ees2 : Float
+ees2 = 77.78
+
+e2 : Float
+e2 = 82.41
+
+f2 : Float
+f2 = 87.31
+
+fis2 : Float
+fis2 = 92.50
+
+ges2 : Float
+ges2 = 92.50
+
+g2 : Float
+g2 = 98.00
+
+gis2 : Float
+gis2 = 103.83
+
+aes2 : Float
+aes2 = 103.83
+
+a2 : Float
+a2 = 110.00
+
+ais2 : Float
+ais2 = 116.54
+
+bes2 : Float
+bes2 = 116.54
+
+b2 : Float
+b2 = 123.47
+
+c3 : Float
+c3 = 130.81
+
+cis3 : Float
+cis3 = 138.59
+
+des3 : Float
+des3 = 138.59
+
+d3 : Float
+d3 = 146.83
+
+dis3 : Float
+dis3 = 155.56
+
+ees3 : Float
+ees3 = 155.56
+
+e3 : Float
+e3 = 164.81
+
+f3 : Float
+f3 = 174.61
+
+fis3 : Float
+fis3 = 185.00
+
+ges3 : Float
+ges3 = 185.00
+
+g3 : Float
+g3 = 196.00
+
+gis3 : Float
+gis3 = 207.65
+
+aes3 : Float
+aes3 = 207.65
+
+a3 : Float
+a3 = 220.00
+
+ais3 : Float
+ais3 = 233.08
+
+bes3 : Float
+bes3 = 233.08
+
+b3 : Float
+b3 = 246.94
+
+c4 : Float
+c4 = 261.63
+
+cis4 : Float
+cis4 = 277.18
+
+des4 : Float
+des4 = 277.18
+
+d4 : Float
+d4 = 293.66
+
+dis4 : Float
+dis4 = 311.13
+
+ees4 : Float
+ees4 = 311.13
+
+e4 : Float
+e4 = 329.63
+
+f4 : Float
+f4 = 349.23
+
+fis4 : Float
+fis4 = 369.99
+
+ges4 : Float
+ges4 = 369.99
+
+g4 : Float
+g4 = 392.00
+
+gis4 : Float
+gis4 = 415.30
+
+aes4 : Float
+aes4 = 415.30
+
+a4 : Float
+a4 = 440.00
+
+ais4 : Float
+ais4 = 466.16
+
+bes4 : Float
+bes4 = 466.16
+
+b4 : Float
+b4 = 493.88
+
+c5 : Float
+c5 = 523.25
+
+cis5 : Float
+cis5 = 554.37
+
+des5 : Float
+des5 = 554.37
+
+d5 : Float
+d5 = 587.33
+
+dis5 : Float
+dis5 = 622.25
+
+ees5 : Float
+ees5 = 622.25
+
+e5 : Float
+e5 = 659.25
+
+f5 : Float
+f5 = 698.46
+
+fis5 : Float
+fis5 = 739.99
+
+ges5 : Float
+ges5 = 739.99
+
+g5 : Float
+g5 = 783.99
+
+gis5 : Float
+gis5 = 830.61
+
+aes5 : Float
+aes5 = 830.61
+
+a5 : Float
+a5 = 880.00
+
+ais5 : Float
+ais5 = 932.33
+
+bes5 : Float
+bes5 = 932.33
+
+b5 : Float
+b5 = 987.77
+
+c6 : Float
+c6 = 1046.50
+
+cis6 : Float
+cis6 = 1108.73
+
+des6 : Float
+des6 = 1108.73
+
+d6 : Float
+d6 = 1174.66
+
+dis6 : Float
+dis6 = 1244.51
+
+ees6 : Float
+ees6 = 1244.51
+
+e6 : Float
+e6 = 1318.51
+
+f6 : Float
+f6 = 1396.91
+
+fis6 : Float
+fis6 = 1479.98
+
+ges6 : Float
+ges6 = 1479.98
+
+g6 : Float
+g6 = 1567.98
+
+gis6 : Float
+gis6 = 1661.22
+
+aes6 : Float
+aes6 = 1661.22
+
+a6 : Float
+a6 = 1760.00
+
+ais6 : Float
+ais6 = 1864.66
+
+bes6 : Float
+bes6 = 1864.66
+
+b6 : Float
+b6 = 1975.53
+
+c7 : Float
+c7 = 2093.00
+
+cis7 : Float
+cis7 = 2217.46
+
+des7 : Float
+des7 = 2217.46
+
+d7 : Float
+d7 = 2349.32
+
+dis7 : Float
+dis7 = 2489.02
+
+ees7 : Float
+ees7 = 2489.02
+
+e7 : Float
+e7 = 2637.02
+
+f7 : Float
+f7 = 2793.83
+
+fis7 : Float
+fis7 = 2959.96
+
+ges7 : Float
+ges7 = 2959.96
+
+g7 : Float
+g7 = 3135.96
+
+gis7 : Float
+gis7 = 3322.44
+
+aes7 : Float
+aes7 = 3322.44
+
+a7 : Float
+a7 = 3520.00
+
+ais7 : Float
+ais7 = 3729.31
+
+bes7 : Float
+bes7 = 3729.31
+
+b7 : Float
+b7 = 3951.07
+
+c8 : Float
+c8 = 4186.01
+
+cis8 : Float
+cis8 = 4434.92
+
+des8 : Float
+des8 = 4434.92
+
+d8 : Float
+d8 = 4698.63
+
+dis8 : Float
+dis8 = 4978.03
+
+ees8 : Float
+ees8 = 4978.03
+
+e8 : Float
+e8 = 5274.04
+
+f8 : Float
+f8 = 5587.65
+
+fis8 : Float
+fis8 = 5919.91
+
+ges8 : Float
+ges8 = 5919.91
+
+g8 : Float
+g8 = 6271.93
+
+gis8 : Float
+gis8 = 6644.88
+
+aes8 : Float
+aes8 = 6644.88
+
+a8 : Float
+a8 = 7040.00
+
+ais8 : Float
+ais8 = 7458.62
+
+bes8 : Float
+bes8 = 7458.62
+
+b8 : Float
+b8 = 7902.13
