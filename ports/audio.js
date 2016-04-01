9b982ff29d031d1b81fe79e24664a30e0cb0edde
Oskar Ehnstrom <oskar@ehnstrom.fi>
2016-04-01 16:18:57 +0300

    Switch between saw and bass sounds based on planet radius

--- ports/audio.js
+++ ports/audio.js
@@ -1,7 +1,16 @@
 function setupAudio(solarsystem) {
 	solarsystem.ports.audio.subscribe(audio);
 
-	var saw = new Wad({source : 'sawtooth'});
+	var saw = new Wad({
+		source : 'sawtooth',
+		env: {
+			attack: 0.03,
+			decay: 0.02,
+			sustain: 1.0,
+			hold: 0.3,
+			release: 0.1
+		}
+	});
 	var bass = new Wad({
 			    source : 'sine',
 			    env : {
@@ -10,25 +19,29 @@ function setupAudio(solarsystem) {
 			        sustain : 0.9,
 			        hold : 0.4,
 			        release : 0.1
-			    },
-			    filter : {
-			        type : 'bandpass',
-			        frequency : 100,
-			        q : 0.180
 			    }
 			});
-	function playNote(freq) {
-		console.log('playNote: ' + freq);
+
+	function getInstrument(instrument) {
+		if (instrument === 'saw') {
+			return saw;
+		} else {
+			return bass;
+		}
+	}
+
+	function playNote(freq, instrument) {
+		console.log('playNote: ' + freq + ' using: ' + instrument);
 	  if (freq > 20 && freq < 20000){
-	    bass.play({pitch: freq});
+	    getInstrument(instrument).play({pitch: freq});
 	  } else {
 	    console.log('Frequency is outside human hearing: ' + freq);
 	  }
 	}
+
 	function audio(freqs) {
-		console.log('audio.js: ' + freqs);
 		for (var i = 0; i < freqs.length; i++) {
-			playNote(freqs[i]);
+			playNote(freqs[i][0], freqs[i][1]);
 		}
 	}
 }
